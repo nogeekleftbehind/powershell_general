316cd8b9abc1b56bc6a100bbac03d63b3ca5fccb
nogeekleftbehind <timbarrett@nogeekleftbehind.com>
2015-03-16 16:46:26 -0400

    Active Directory PowerShell scripts

---
+++ ActiveDirectory/Get-ServerLastLogon.ps1
@@ -0,0 +1,18 @@
+﻿<#
+.Synopsis
+   Get-ServerLastLogon - Returns MachineName and LastLogon
+.DESCRIPTION
+   Script searches Active Directory for all 'Server' objects 
+   Displays a list of the results
+.EXAMPLE
+   Get-ServerLastLogon
+#>
+
+# Find all servers in Active Directory
+$dcs = Get-ADComputer -Filter { OperatingSystem -Like '*Server*' } -Properties OperatingSystem
+
+# Check each server for last logon, display date and time
+ForEach($dc in $dcs) { 
+    Get-ADComputer $dc.Name -Properties lastlogontimestamp | 
+    Select-Object @{n="Computer";e={$_.Name}}, @{Name="Lastlogon"; Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}}
+}
