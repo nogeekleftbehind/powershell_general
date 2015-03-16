c993947aff7e9bbcdf94ff9c742951cea89d9984
nogeekleftbehind <timbarrett@nogeekleftbehind.com>
2015-03-16 17:02:09 -0400

    Active Directory PowerShell scripts

---
+++ ActiveDirectory/Show-FSMORoles.ps1
@@ -0,0 +1,18 @@
+﻿<#
+.Synopsis
+   Show-FSMORoles - Show all five FSMO role holders
+.DESCRIPTION
+   Polls local computer for domain information.
+   Uses that domain info to display current FSMO role holders.
+.EXAMPLE
+   Show-FSMORoles
+#>
+
+# Get current domain 
+$Domain = Get-ADDomain
+
+# Get DNSRoot of current domain
+$DNSRoot = $Domain.DNSRoot
+
+# Use variables above to display all 5 FSMO role holders
+Netdom query /domain:$DNSRoot FSMO
