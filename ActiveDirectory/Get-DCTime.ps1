67f6d35c5b21512753dc068b486a306ed2f6729a
nogeekleftbehind <timbarrett@nogeekleftbehind.com>
2015-03-17 10:32:28 -0400

    Updated 2 AD scripts

---
+++ ActiveDirectory/Get-DCTime.ps1
@@ -0,0 +1,77 @@
+﻿<#
+.Synopsis
+   Get-DCTime - Compares time on AD Domain Controllers
+.DESCRIPTION
+   Scripts polls for AD for domain controllers, then polls each DC for current time.
+   This script takes a while to run if you have a large number of DCs.
+   Script requires the use of the Get-Time function
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+
+function Get-Time {
+	<#
+		.SYNOPSIS
+			Gets the time of a windows server
+
+		.DESCRIPTION
+			Uses WMI to get the time of a remote server
+
+		.PARAMETER  ServerName
+			The Server to get the date and time from
+
+		.EXAMPLE
+			PS C:\> Get-Time localhost
+
+		.EXAMPLE
+			PS C:\> Get-Time server01.domain.local -Credential (Get-Credential)
+
+	#>
+	[CmdletBinding()]
+	param(
+		[Parameter(Position=0, Mandatory=$true)]
+		[ValidateNotNullOrEmpty()]
+		[System.String]
+		$ServerName,
+
+		$Credential
+
+	)
+	try {
+			If ($Credential) {
+				$DT = Get-WmiObject -Class Win32_LocalTime -ComputerName $servername -Credential $Credential
+			} Else {
+				$DT = Get-WmiObject -Class Win32_LocalTime -ComputerName $servername
+			}
+	}
+	catch {
+		throw
+	}
+
+	$Times = New-Object PSObject -Property @{
+		ServerName = $DT.__Server
+		DateTime = (Get-Date -Day $DT.Day -Month $DT.Month -Year $DT.Year -Minute $DT.Minute -Hour $DT.Hour -Second $DT.Second)
+	}
+	$Times
+
+}
+
+#Example of using this function
+$Servers = Get-ADComputer -Filter {OperatingSystem -Like "Windows Server*"} | Sort-Object -Property @{Expression="Name";Descending=$False}
+
+$Servers | Foreach {
+	Get-Time $_.Name
+}
+
+# Get all DCs, load into variable $Servers
+
+    $Servers = Get-ADDomainController -Filter *
+
+# Loop through all DCs, poll each for current time, output to screen
+
+foreach ($Server in $Servers)
+{
+    Get-Time $Server 
+}
--- ActiveDirectory/Get-ServerLastLogon.ps1
+++ ActiveDirectory/Get-ServerLastLogon.ps1
@@ -1,18 +1,21 @@
 ﻿<#
 .Synopsis
-   Get-ServerLastLogon - Returns MachineName and LastLogon
+   Get-ServerLastLogon - Show last logon time and date for all servers
 .DESCRIPTION
-   Script searches Active Directory for all 'Server' objects 
-   Displays a list of the results
+   Script searches Active Directory for all 'Server' objects
+   Returns MachineName and LastLogon
+   Outputs to screen
+Long description
 .EXAMPLE
    Get-ServerLastLogon
 #>
 
-# Find all servers in Active Directory
-$dcs = Get-ADComputer -Filter { OperatingSystem -Like '*Server*' } -Properties OperatingSystem
+# Find all Servers in Active Directory
+$Servers = Get-ADComputer -Filter { OperatingSystem -Like '*Server*' } -Properties OperatingSystem
 
-# Check each server for last logon, display date and time
-ForEach($dc in $dcs) { 
-    Get-ADComputer $dc.Name -Properties lastlogontimestamp | 
+# Loop through all servers, display last logon time and date
+foreach($Server in $Servers) 
+{ 
+    Get-ADComputer $Server.Name -Properties lastlogontimestamp | 
     Select-Object @{n="Computer";e={$_.Name}}, @{Name="Lastlogon"; Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}}
 }
