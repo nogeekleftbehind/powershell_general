34997b12912c2f3d89564e6a13a48511ca37de0f
nogeekleftbehind <timbarrett@nogeekleftbehind.com>
2015-03-17 10:09:09 -0400

    Added Get-PasswordExpiration script

---
+++ ActiveDirectory/Get-PasswordExpiration.ps1
@@ -0,0 +1,14 @@
+﻿<#
+.Synopsis
+   Get-PasswordExpiration - Show users with expiring passwords
+.DESCRIPTION
+   Polls Active Directory for a list of users whose passwords expire.
+   Shows DisplayName, ExpiryDate, WhenCreated & LastLogonDate in a table
+   Outputs to screen
+.EXAMPLE
+   Get-PasswordExpiration
+#>
+
+Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} –Properties DisplayName,WhenCreated,LastLogonDate,"msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "DisplayName",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}},WhenCreated,LastLogonDate 
+
+ 
