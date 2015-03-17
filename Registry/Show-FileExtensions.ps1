6b7e2ecef6919b9b0b8367c931e70125aac00288
nogeekleftbehind <timbarrett@nogeekleftbehind.com>
2015-03-17 10:54:44 -0400

    Added 2 Registry scripts and 1 AD script

---
+++ Registry/Show-FileExtensions.ps1
@@ -0,0 +1,14 @@
+﻿<#
+.Synopsis
+   Show-FileExtensions - Shows file extensions in Windows Explorer
+.DESCRIPTION
+   Changes the registry value to show hidden file extensions in Windows Explorer
+.EXAMPLE
+   Show-FileExtensions
+#>
+
+	Set-ItemProperty -Name HideFileExt -Path "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\" -Value "0"
+<#
+	0 = Show File Extensions
+	1 = Hide File Extensions
+#>
---
+++ Registry/Show-FullPath.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.Synopsis
+   Show-FullPath - Enables Full Path in the title bar
+.DESCRIPTION
+   Show Windows Explorer Full Path in the Windows Explorer title bar
+.EXAMPLE
+   Show-FullPath
+#>
+
+    Set-ItemProperty -Name FullPath -Path "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState\" -Value "1"
+
+<#
+	0 = Show Folder Name Only
+	1 = Show Full Path in Title Bar
+#>
